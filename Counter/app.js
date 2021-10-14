0c48dcdd0d5ca52fb8b5470ea7775e2c61b8f7bf
Steven Delane Glover <47308332+Mephisto-234@users.noreply.github.com>
2021-10-14 13:59:29 -0700

    -

---
+++ Counter/app.js
@@ -0,0 +1,11 @@
+// set initial count
+let count = 0
+
+// select value and button, '#' for ids, '.' for classes
+
+const value = document.querySelector('#value')
+const btns = document.querySelector('.btn')
+
+btns.forEach(function (item) {
+    console.log()
+})
